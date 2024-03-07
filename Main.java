43b7477f8387b3f272600460d767d334697e50d5
bimorajendraa <elitamelati08@gmail.com>
2024-03-07 16:50:40 +0700

    input

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("    /\\_____/\\   ");
+        System.out.println("   /  o    o \\  ");
+        System.out.println("  (  == ^ ==  )  ");
+        System.out.println("   )         (   ");
+        System.out.println("  (           )  ");
+        System.out.println(" ( (  )   (  ) ) ");
+        System.out.println("(__(__)___(__)__)");
+    }
+}
